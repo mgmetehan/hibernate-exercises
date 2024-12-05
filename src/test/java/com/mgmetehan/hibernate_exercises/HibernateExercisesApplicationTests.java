8af173ce01486e132a490f4c6d55e1fafe6d5b6d
mgmetehan <mgmetehan@hotmail.com>
2024-12-05 21:01:51 +0300

    init project

---
+++ src/test/java/com/mgmetehan/hibernate_exercises/HibernateExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.hibernate_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
